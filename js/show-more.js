3be90190eb8fae53bd91c865488eb631fe1815da
softuneed <121585093+softuneed@users.noreply.github.com>
2022-12-29 01:13:49 +0200

    Add files via upload

---
+++ js/show-more.js
@@ -0,0 +1,15 @@
+const showMore = document.querySelector('.show-more');
+const productsLength = document.querySelectorAll('.programs__item').length;
+let items = 7;
+
+showMore.addEventListener('click', () => {
+	items += 7;
+	const array = Array.from(document.querySelector('.programs__content').children);
+	const visItems = array.slice(0, items);
+
+	visItems.forEach(el => el.classList.add('is-visible'));
+
+	if (visItems.length === productsLength) {
+		showMore.style.display = 'none';
+	}
+});

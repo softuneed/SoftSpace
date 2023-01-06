1cb223e5b25d3089eedb198813231e52bc11b2b7
softuneed <121585093+softuneed@users.noreply.github.com>
2023-01-06 13:07:49 +0200

    Add files via upload

---
+++ js/openLink.js
@@ -0,0 +1,20 @@
+window.addEventListener('click', function (event) {
+
+   if (event.target.hasAttribute('data-search')) {
+      items += 10;
+      const array = Array.from(document.querySelector('.programs__content').children);
+      const visItems = array.slice(0, items);
+
+      visItems.forEach(el => el.classList.add('is-visible'));
+
+      if (visItems.length === productsLength) {
+         showMore.style.display = 'none';
+      }
+  
+   } 
+
+});
+
+   
+
+

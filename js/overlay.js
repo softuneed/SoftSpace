d02bc1a7658aedac4e2c6645c7148c61223f4ae8
softuneed <121585093+softuneed@users.noreply.github.com>
2023-01-07 11:49:28 +0200

    Add files via upload

---
+++ js/overlay.js
@@ -0,0 +1,24 @@
+const overlay = document.querySelector('.more__overlay');
+const infoMore = document.querySelector('.info__more');
+const bodyk = document.querySelector('.body')
+
+window.addEventListener('click', function (event) {
+
+   if (event.target.hasAttribute('data-link')) {
+      overlay.classList.add('overlay__active');
+   } 
+   
+   else if (event.target.hasAttribute('data-popup')) {
+      overlay.classList.add('overlay__active');
+   }
+
+   else if (event.target.hasAttribute('data-text')) {
+      overlay.classList.add('overlay__active');
+   }
+
+   else overlay.classList.remove('overlay__active');
+
+});
+
+
+ 

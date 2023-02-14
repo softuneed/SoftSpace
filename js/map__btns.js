d6a97fc0ca8875e7b968007653ed2d3cc0dc8815
softuneed <121585093+softuneed@users.noreply.github.com>
2023-02-14 23:14:03 +0200

    Delete js directory

--- js/map__btns.js
+++
@@ -1,7 +0,0 @@
-const elem = document.querySelectorAll('.list__item--wrapper #listItem');
-for(i=0;i<elem.length;i++){
-   elem[i].onclick = funcShow;
-}
-function funcShow(){
-   this.classList.toggle('list__item--minus')
-}

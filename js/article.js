eee171c097991922c3d04abb8f2c33925504cbb6
softuneed <121585093+softuneed@users.noreply.github.com>
2023-05-22 00:31:53 +0300

    Delete js directory

--- js/article.js
+++
@@ -1,8 +0,0 @@
-const more = document.querySelectorAll('.article__text');
-
-for(i=0;i<more.length;i++){
-   more[i].onclick = funcOpen;
-}
-function funcOpen(){
-   this.classList.toggle('article__text--full')
-}
